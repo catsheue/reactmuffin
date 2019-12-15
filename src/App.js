052937c2486ea460313c28039444587f9fe2358b
Luna Goodnight <catsheue@gmail.com>
2019-12-15 21:43:09 +0800

    init commit :tada:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import './app.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Emoji from 'a11y-react-emoji';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <header className="app__header">
+          <Emoji symbol="🎄" />
+            ...React Rocks...
+          <Emoji symbol="🎅🏽" />
+        </header>
+        <div className="app__article cf">
+          <div className="app__nav">
+            <ul>
+              <li>
+                <Link to="/">
+                <Emoji symbol="🍄" />
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Emoji symbol="🍄" />
+              </li>
+            </ul>
+          </div>
+          <div className="app__content">
+            <Switch>
+
+
+              <Route path="/">
+                <Home />
+              </Route>
+          </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
+function Home() {
+  return (<>
+    <h2>Welcome to my React Rocks project.</h2>
+    <div>It's all about examples write in React.</div>
+  </>);
+
+}
 export default App;
